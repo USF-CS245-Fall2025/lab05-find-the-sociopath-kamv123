b7d82062d55bd5535be59e569801bae2c59d24fe
Kameron V <66443784+kamv123@users.noreply.github.com>
2025-11-24 23:09:47 -0800

    Add findTheSociopath method implementation
    Implement findTheSociopath method to determine the sociopath in a group based on likes received and given.

--- sociopath/app/src/main/java/Sociopath.java
+++ sociopath/app/src/main/java/Sociopath.java
@@ -3,7 +3,28 @@ import java.util.List;
 
 public class Sociopath {
 
-	public int findTheSociopath (int groupSize, List<int []> likeList) {
-		return 0;
-	}
+    public int findTheSociopath (int groupSize, List<int []> likeList) {
+        if (groupSize <= 0) return -1;
+
+        int[] likesReceived = new int[groupSize + 1];
+        int[] likesGiven = new int[groupSize + 1];
+
+        for (int[] pair : likeList){
+            int likes = pair[0];
+            int likedby = pair[1];
+
+            if (likes <= 0 || likes > groupSize || likedby <= 0 || likedby > groupSize ) return -1;
+
+            likesGiven[likes]++;
+            likesReceived[likedby]++;
+        }
+
+
+        for (int i = 1; i <= groupSize; i++) {
+            if (likesGiven[i] == 0 && likesReceived[i] == groupSize-1) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
